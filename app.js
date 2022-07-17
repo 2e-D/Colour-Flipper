7fd12a05d818c0e69baa11a58121d37213527f5d
Dan Toohey <100450914+2e-D@users.noreply.github.com>
2022-07-17 20:20:38 +0100

    Added JS functionality
    The page background color now changes color and displays the color name when the button is clicked

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-const colors = ["green", "red", "rgba(133,122,200)",
-"#f15025"]
+const colors = ["Green", "Red", "Cyan",
+"Yellow","Orange", "Pink", "Purple", "Oops this doesn't display a new color"];
 
-const btn = document.getElementById("btn")
+const btn = document.getElementById('btn')
+const color = document.querySelector(".color");
+
+
+btn.addEventListener("click",function (){
+    // Need to get random number
+    const randomNumber = getRandomNumber();
+console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+    
+});
+
+function getRandomNumber () {
+    return Math.floor(Math.random() * colors.length);
+}
